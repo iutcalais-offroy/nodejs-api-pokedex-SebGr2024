cc7593800f66f8c40e3954afdec1b003503c73c0
GROUE_sebastien <sebgroue@gmail.com>
2026-02-06 10:10:20 +0100

    Ajout de la fonctionnalité Sign-in avec toutes les fonctionnalités, puis j'ai repris le code du cours pour le middleware et je l'ai adapté puis ajout route sign-in dans auth.route.ts

---
+++ src/auth/middleware/auth.middleware.ts
@@ -0,0 +1,37 @@
+import {NextFunction, Request, Response} from 'express'
+import jwt from 'jsonwebtoken'
+
+declare global {
+    namespace Express {
+        interface Request {
+            userId?: number
+        }
+    }
+}
+
+export const authenticateToken = (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+
+    const authHeader = req.headers.authorization
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (!token) {
+        return res.status(401).json({error: 'Token manquant'})
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
+            userId: number
+            email: string
+        }
+
+        req.userId = decoded.userId
+
+        next()
+    } catch (error) {
+        return res.status(401).json({error: 'Token invalide ou expiré'})
+    }
+}
--- src/auth/routes/auth.routes.ts
+++ src/auth/routes/auth.routes.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import { signUp } from "../controllers/sign-up";
+import { signIn } from "../controllers/sign-in";
+import { authenticateToken } from "../middleware/auth.middleware"; /* Y a rien a protéger pour l'instant mais je l'insére déjà pour préparer le terrain  */
+
+
 
 const router = express.Router();
 
 router.post("/sign-up", signUp);
-
+router.post("/sign-in", signIn);
 export default router;
