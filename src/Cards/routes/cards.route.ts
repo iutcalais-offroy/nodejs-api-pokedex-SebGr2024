85d345940164c144630c4e6f36a8997545941a53
GROUE_sebastien <sebgroue@gmail.com>
2026-02-24 15:33:52 +0100

    docs(cards): Ajout de la documentation JSDoc dans les cards

--- src/Cards/routes/cards.route.ts
+++ src/Cards/routes/cards.route.ts
@@ -3,6 +3,13 @@ import { getCards } from '../controllers/cards.controller'
 
 const router = express.Router()
 
+/**
+ * @route GET api/cards
+ *
+ * Récupère les cartes enregistrée
+ *
+ * @returns {Promise<Response>} Retourne les cartes créé avec un statut 200
+ */
 router.get('/', getCards)
 
 export default router
