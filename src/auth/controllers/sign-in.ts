ad6ae642efa40db5073dd649c70ce482e7080292
GROUE_sebastien <sebgroue@gmail.com>
2026-02-24 14:45:22 +0100

    docs(auth): Ajout de la documentation JSDoc sur les fichiers du dossiers auth

--- src/auth/controllers/sign-in.ts
+++ src/auth/controllers/sign-in.ts
@@ -3,6 +3,24 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { prisma } from './../../database'
 
+/**
+ * Se connecter en tant qu'utilisateur
+ *
+ * @route POST api/auth/sign-in
+ *
+ * @async
+ * @param {Request} req Requête Express
+ * @param {Response} res Réponse Express
+ *
+ *
+ * @param {SignIn} req.body Données à rentrer pour se connecter
+ *
+ * @returns {Promise<Response>} Pour nous dire que l'utilisateur est connecter avec un code 200 avec son token qu'il faudra tout le temps saisir
+ *
+ * @throws {400} Si aucun email et password n'est saisie
+ * @throws {401} Si l'email et le mot de passe est incorrect
+ * @throws {500} On renvoie erreur serveur
+ */
 export const signIn = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body
@@ -19,7 +37,7 @@ export const signIn = async (req: Request, res: Response) => {
 
     if (!user) {
       return res.status(401).json({
-        message: 'Email ou incorrect',
+        message: 'Email incorrect',
       })
     }
 
--- src/auth/routes/auth.routes.ts
+++ src/auth/routes/auth.routes.ts
@@ -3,7 +3,26 @@ import { signUp } from '../controllers/sign-up'
 import { signIn } from '../controllers/sign-in'
 
 const router = express.Router()
+/**
+ * @route POST api/auth/sign-up
+ *
+ * S'inscrire (se crée un compte) en tant qu'utilisateur
+ *
+ * @param {SignUp} req.body Données à rentrer pour se crée un compte
+ *
+ * @returns {Promise<Response>} Pour nous dire que l'utilisateur est crée avec un code 201 avec son token qu'il faudra tout le temps saisir
+ */
 
 router.post('/sign-up', signUp)
+
+/**
+ * @route POST api/auth/sign-in
+ *
+ * Se connecter en tant qu'utilisateur
+ *
+ * @param {SignIn} req.body Données à rentrer pour se connecter
+ *
+ * @returns {Promise<Response>} Pour nous dire que l'utilisateur est connecter avec un code 200 avec son token qu'il faudra tout le temps saisir
+ */
 router.post('/sign-in', signIn)
 export default router
